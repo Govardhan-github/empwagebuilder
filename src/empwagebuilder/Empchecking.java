9cfecf5505a3fb99a23a06b4e1d925962b83ff66
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-11 22:02:54 -0700

    Emp attendence added

--- src/empwagebuilder/Empchecking.java
+++ src/empwagebuilder/Empchecking.java
@@ -4,8 +4,16 @@ public class Empchecking {
 
 	public static void main(String[] args) 
 	{
-		
-		System.out.println("welcome to the Empwagebuild programme");
+	//constants
+	int IS_FULL_TIME = 1;
+	System.out.println("welcome to the Empwagebuild programme");
+	//Computation
+	double empcheck = Math.floor(Math.random() * 10) % 2;
+	if ( empcheck == IS_FULL_TIME )
+		System.out.println("Emp is present");
+	else 
+		System.out.println("Emp is absent");
 	}
+	
 
 }
