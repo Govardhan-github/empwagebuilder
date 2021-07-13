cd68dcf68f1d0415320401d55602aad95b26fa38
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-12 20:31:07 -0700

    UC2 Emp wage added with full time

--- src/empwagebuilder/Empchecking.java
+++ src/empwagebuilder/Empchecking.java
@@ -6,13 +6,18 @@ public class Empchecking {
 	{
 	//constants
 	int IS_FULL_TIME = 1;
+	int EMP_RATE_PER_HR=20;
+	int emphrs=0;
+	int empwage=0;
 	System.out.println("welcome to the Empwagebuild programme");
 	//Computation
 	double empcheck = Math.floor(Math.random() * 10) % 2;
 	if ( empcheck == IS_FULL_TIME )
-		System.out.println("Emp is present");
-	else 
-		System.out.println("Emp is absent");
+		emphrs=8;
+	else
+		emphrs=0;
+		empwage = emphrs * EMP_RATE_PER_HR;
+		System.out.println("Emp wage:" +empwage);
 	}
 	
 
