28d7e330fa04636ef192c94b1f6799367479dc7f
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-12 20:57:09 -0700

    UC3_PART_TIME_ADDED

--- src/empwagebuilder/Empchecking.java
+++ src/empwagebuilder/Empchecking.java
@@ -5,19 +5,21 @@ public class Empchecking {
 	public static void main(String[] args) 
 	{
 	//constants
-	int IS_FULL_TIME = 1;
+	int IS_FULL_TIME = 2;
+	int IS_PART_TIME = 1;
 	int EMP_RATE_PER_HR=20;
 	int emphrs=0;
 	int empwage=0;
 	System.out.println("welcome to the Empwagebuild programme");
 	//Computation
-	double empcheck = Math.floor(Math.random() * 10) % 2;
+	double empcheck = Math.floor(Math.random() * 10) % 3;
 	if ( empcheck == IS_FULL_TIME )
 		emphrs=8;
-	else
-		emphrs=0;
-		empwage = emphrs * EMP_RATE_PER_HR;
-		System.out.println("Emp wage:" +empwage);
+	
+	else if ( empcheck == IS_PART_TIME )
+		emphrs=4;
+	empwage = emphrs * EMP_RATE_PER_HR;
+	System.out.println("Emp wage:" +empwage);
 	}
 	
 
