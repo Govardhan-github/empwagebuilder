52a63df44f72773317fe2baece335955023a2ffc
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-12 21:44:52 -0700

    UC4 WORKING DAYS ADDED

--- src/empwagebuilder/Empchecking.java
+++ src/empwagebuilder/Empchecking.java
@@ -5,13 +5,16 @@ public class Empchecking {
 	public static final int IS_PART_TIME = 1 ;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
+	public static final int NO_OF_WORKING_DAYS = 2;
 	
 	public static void main(String[] args) 
 	{
 	int emphrs=0;
 	int empwage=0;
+	int totalEmpwage=0;
 	System.out.println("welcome to the Empwagebuild programme");
 	//Computation
+	for ( int day = 0; day < NO_OF_WORKING_DAYS; day++) {
 	int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 	switch (empcheck) {
 	 	case IS_FULL_TIME:
@@ -24,8 +27,11 @@ public class Empchecking {
 			emphrs=0;
 	}
 	empwage = emphrs * EMP_RATE_PER_HR;
+	totalEmpwage += empwage;
+	
 	System.out.println("Emp wage:" +empwage);
 	}
-	
+System.out.println("Total Emp Wage: " +totalEmpwage);
 
+	}
 }
