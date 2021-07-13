230dd404895d26ae30d599cc58b4eae10176cb8b
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-12 21:33:36 -0700

    UC4 Switch case added

--- src/empwagebuilder/Empchecking.java
+++ src/empwagebuilder/Empchecking.java
@@ -1,23 +1,28 @@
 package empwagebuilder;
 
 public class Empchecking {
-
+	//constants
+	public static final int IS_PART_TIME = 1 ;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HR = 20;
+	
 	public static void main(String[] args) 
 	{
-	//constants
-	int IS_FULL_TIME = 2;
-	int IS_PART_TIME = 1;
-	int EMP_RATE_PER_HR=20;
 	int emphrs=0;
 	int empwage=0;
 	System.out.println("welcome to the Empwagebuild programme");
 	//Computation
-	double empcheck = Math.floor(Math.random() * 10) % 3;
-	if ( empcheck == IS_FULL_TIME )
-		emphrs=8;
-	
-	else if ( empcheck == IS_PART_TIME )
-		emphrs=4;
+	int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+	switch (empcheck) {
+	 	case IS_FULL_TIME:
+			emphrs=8;
+			break;
+	 	case IS_PART_TIME:
+	 		emphrs=4;
+			break;
+		default:
+			emphrs=0;
+	}
 	empwage = emphrs * EMP_RATE_PER_HR;
 	System.out.println("Emp wage:" +empwage);
 	}
