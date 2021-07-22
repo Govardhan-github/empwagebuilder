6f19531791f55801ab9222f37f712eaa1305d69b
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-22 02:14:02 -0700

    ADDED : Total Wage For Each Company

--- src/empwagebuilder/Empchecking.java
+++ src/empwagebuilder/Empchecking.java
@@ -5,14 +5,28 @@ public class Empchecking {
 	 * Program To Calculate Employee Wage
 	 */
 	
-	//constants
+	//
 	public static final int IS_PART_TIME = 0 ;
 	public static final int IS_FULL_TIME = 1;
 
+	private final String company;
+	private final int empRatePerHr;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private static int totalempwage;
+	
+	public Empchecking(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth)
+	{
+		this.company = company;
+		this.empRatePerHr = empRatePerHr;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+				
+	}
 	/*
 	 * Declaring Method To Calculate EmpWage
 	 */
-	public static int computeEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth) 
+	public void computeEmpWage() 
 	
 	{
 	int emphrs=0;
@@ -20,8 +34,10 @@ public class Empchecking {
 	int totalworkingdays=0;
 	System.out.println("welcome to the Empwagebuild programme");
 	//While Condition To Calculate Total EmpWage
-	while ( totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWorkingDays) {
-		totalworkingdays++;
+	while ( totalemphrs <= maxHoursPerMonth && totalworkingdays < numOfWorkingDays) 
+		{
+			totalworkingdays++;
+		
 	int empcheck = (int) Math.floor(Math.random() * 10) % 2;
 	switch (empcheck) {
 	 	case IS_FULL_TIME:
@@ -33,19 +49,28 @@ public class Empchecking {
 		default:
 			emphrs=0;
 	}
-	totalemphrs=emphrs;
+	totalemphrs  =emphrs;
 	System.out.println("Days:" + totalworkingdays + "emphr: " +emphrs);
+		}
+	totalempwage = totalemphrs * empRatePerHr;
+		}
+	
+	@Override
+	public String toString() {
+		return "Total Emp Wage For Company: " +company+" is : " + totalempwage ;
 	}
-	int totalempwage = totalemphrs * empRatePerHr;
-	System.out.println("Total Emp Wage For Company: " +company+" is : " +totalempwage);
-	return totalempwage;
-	}
+	
+	
 	/*
 	 * Declaring Main Method 
 	 * Calling ComputeEmpWage Method Here
 	 */
 	public static void main(String[] args) {
-		computeEmpWage("Dmart",20,20,50);
-		computeEmpWage("Relaince",30,40,70);
+		Empchecking dMart = new Empchecking("DMart" , 20, 2,10);
+		Empchecking reliance = new Empchecking("Reliance" , 10, 4, 20);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
 }
