d28ac1f4aa76e174efefd9291a406b13ca93d194
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-21 23:51:08 -0700

    Refactor The Code Using Method And Class

--- src/empwagebuilder/Empchecking.java
+++ src/empwagebuilder/Empchecking.java
@@ -1,20 +1,26 @@
 package empwagebuilder;
 
 public class Empchecking {
+	/*
+	 * Program To Calculate Employee Wage
+	 */
+	
 	//constants
 	public static final int IS_PART_TIME = 1 ;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HR = 20;
 	public static final int NO_OF_WORKING_DAYS = 20;
 	public static final int MAX_NO_HRS = 100;
-	
-	public static void main(String[] args) 
+	/*
+	 * Declaring Method To Calculate EmpWage
+	 */
+	public static int computeEmpWage() 
 	{
 	int emphrs=0;
 	int totalemphrs=0;
 	int totalworkingdays=0;
 	System.out.println("welcome to the Empwagebuild programme");
-	//Computation
+	//While Condition To Calculate Total EmpWage
 	while ( totalemphrs <= MAX_NO_HRS && totalworkingdays < NO_OF_WORKING_DAYS) {
 		totalworkingdays++;
 	int empcheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -33,13 +39,13 @@ public class Empchecking {
 	}
 	int totalempwage = totalemphrs * EMP_RATE_PER_HR;
 	System.out.println("total emp wage:" +totalempwage);
-	
-	//empwage = emphrs * EMP_RATE_PER_HR;
-	//totalEmpwage += empwage;
-	
-	//System.out.println("Emp wage:" +empwage);
+	return totalempwage;
 	}
-//System.out.println("Total Emp Wage: " +totalEmpwage);
-
+	/*
+	 * Declaring Main Method 
+	 * Calling ComputeEmpWage Method Here
+	 */
+	public static void main(String[] args) {
+		computeEmpWage();
 	}
-
+}
